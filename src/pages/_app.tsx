143d41d7ee38947140ed6476bcd71d8dd4f2e1cf
fadi <fadi99.email@gmail.com>
2023-08-14 20:05:43 +0700

    Feat: Add global styles

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,15 +1,20 @@
 import Layout from "@/components/layout";
 import client from "@/graphql/client";
+import { globalStyles } from "@/styles/global.styles";
 import "@/styles/globals.css";
 import { ApolloProvider } from "@apollo/client";
+import { Global } from "@emotion/react";
 import type { AppProps } from "next/app";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <ApolloProvider client={client}>
-      <Layout>
-        <Component {...pageProps} />
-      </Layout>
-    </ApolloProvider>
+    <>
+      <Global styles={globalStyles} />
+      <ApolloProvider client={client}>
+        <Layout>
+          <Component {...pageProps} />
+        </Layout>
+      </ApolloProvider>
+    </>
   );
 }
---
+++ src/styles/global.styles.ts
@@ -0,0 +1,10 @@
+import { css } from "@emotion/react";
+import theme from "./theme";
+
+const globalStyles = css({
+  body: {
+    color: theme.colors.textPrimary,
+  },
+});
+
+export { globalStyles };
